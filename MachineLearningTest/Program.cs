6996f7023ba710f8bcfe1285bb5863ec6666f29c
Arturo Asto <arturo.asto@hotmail.com>
2020-03-06 17:06:07 -0500

    Add project files.

---
+++ MachineLearningTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using MachineLearningTestML.Model;
+using System;
+
+namespace MachineLearningTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shouldContinue = true;
+            while (shouldContinue)
+            {
+                var input = new ModelInput();
+                input.SentimentText = Console.ReadLine();
+
+                // Load model and predict output of sample data
+                ModelOutput result = ConsumeModel.Predict(input);
+                Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction} and score is: {result.Score}");
+
+                Console.WriteLine("Should continue? Y/N");
+                var response = Console.ReadKey();
+
+                if (response.KeyChar.ToString() == "N") shouldContinue = false; 
+            }
+        }
+    }
+}
